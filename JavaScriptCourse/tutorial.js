5be8eef1abe056062a24f53d8d0d88a8329d8207
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-05-05 20:03:16 +0300

    javascript arrays

--- JavaScriptCourse/tutorial.js
+++ JavaScriptCourse/tutorial.js
@@ -131,4 +131,39 @@
     var vanillaDonut = new Donut("vanilla", true, 10, false);
     console.log(vanillaDonut);// Donut {glazed: true, sweetness: 10, hasChocalate: false, sayType: ƒ, showSweetness: ƒ}
 */
-   
+    // Arrays
+        // Collection of data
+        var myArray = [1, 2, 3, "jsdsds", "sds", true];
+        console.log(myArray);// (6) [1, 2, 3, 'jsdsds', 'sds', true]
+
+        console.log(myArray.length);// 6
+        console.log(myArray[3]);// jsdsds
+
+        var myNamesArray = new Array();
+        myNamesArray[0] = "Jessica";
+        myNamesArray[1] = "Chris";
+        myNamesArray[2] = "Jhon";
+        console.log(myNamesArray);// (3) ['Jessica', 'Chris', 'Jhon']
+
+        myNamesArray.push("Bob");
+        console.log(myNamesArray);// (4) ['Jessica', 'Chris', 'Jhon', 'Bob']
+        myNamesArray.push("Sally", "Kelly");
+        console.log(myNamesArray);// (6) ['Jessica', 'Chris', 'Jhon', 'Bob', 'Sally', 'Kelly']
+        
+        var lastName = myNamesArray.pop();
+        console.log(lastName);// Kelly
+        console.log(myNamesArray);// (5) ['Jessica', 'Chris', 'Jhon', 'Bob', 'Sally']
+
+        var sortedArray = myNamesArray.sort();
+        console.log(sortedArray);// (5) ['Bob', 'Chris', 'Jessica', 'Jhon', 'Sally']
+
+        sortedArray.reverse();
+        console.log(sortedArray);// (5) ['Sally', 'Jhon', 'Jessica', 'Chris', 'Bob']
+
+        var concatedArray = myNamesArray.concat(sortedArray);
+        console.log(concatedArray);// (10) ['Sally', 'Jhon', 'Jessica', 'Chris', 'Bob', 'Sally', 'Jhon', 'Jessica', 'Chris', 'Bob']
+
+        var slicedArray = concatedArray.slice(5);
+        console.log(slicedArray);// (5) ['Sally', 'Jhon', 'Jessica', 'Chris', 'Bob']
+
+        
