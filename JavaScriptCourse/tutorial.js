13c85e12fd6107bd04bda1a07b7401ef9b6a9ecd
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-05-05 19:48:29 +0300

    javascript numbers

---
+++ JavaScriptCourse/tutorial.js
@@ -0,0 +1,20 @@
+// Basic Types
+
+    // numbers
+    /*
+    console.log(10);// prints 10
+    console.log(typeof 10);// prints "number"
+    console.log(typeof "10");// prints "string"
+
+    // we can also use the built-in Math object to perform more complex mathematical operations
+    console.log(Math.random());// prints random number
+    console.log(Math.PI);// prints PI
+
+    console.log("10" + 200);// concatenate them so prints 10200
+
+    console.log(parseInt("10") + 200); // adds them so prints 210
+    console.log(parseInt("hello"));// prints NaN(Not a Number)
+
+    console.log(isNaN("hello"));// prints true (which means is not a number)
+    
+    */
